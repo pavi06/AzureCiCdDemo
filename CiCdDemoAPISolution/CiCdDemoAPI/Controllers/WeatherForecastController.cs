8cf302c94913c64c024765ae3f70a93a07d2a552
Pavithra P <69066762+pavi06@users.noreply.github.com>
2024-07-22 13:12:57 +0530

    Update WeatherForecastController.cs

--- CiCdDemoAPISolution/CiCdDemoAPI/Controllers/WeatherForecastController.cs
+++ CiCdDemoAPISolution/CiCdDemoAPI/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace CiCdDemoAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warmer", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
